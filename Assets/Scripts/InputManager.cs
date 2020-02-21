dfe71296fd8eb87a45f29768118c1340e9899938
bublik <harchevnikovdenis@mail.ru>
2020-02-21 15:54:40 +0300

    changed the final platform and user input script

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,102 +5,56 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class InputManager : MonoBehaviour
 {
-    [SerializeField] private float yRBVelocity;
+    [SerializeField] private float yVelocity;
     private Vector3 velocityVector;
 
-    private bool isGrounded;
-    private int jumpCount;
+    private Rigidbody rigidbody;
 
-    private Rigidbody rb;
-    private float timer;
+    private int jumpCount;
 
     private void Start()
     {
-        velocityVector = Vector3.zero;
-        velocityVector.y = yRBVelocity;
+        rigidbody = GetComponent<Rigidbody>();
 
-        rb = GetComponent<Rigidbody>();
-    }
-    private void FixedUpdate() 
-    {
-        if (!isGrounded)
-            if (isGround())
-                Landing();
-
-        if(Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if (touch.position.y > Screen.height * 0.65f)
-                return;
-
-
-            //Проверка прыжка с земли
-            if (jumpCount == 0 && !isGrounded)
-                return;
-
-            //Пользователь зажал
-            if (jumpCount == 2)
-            {
-                Holding(touch);
-            }
-
-            //Пользователь тапнул
-            if (touch.phase == TouchPhase.Began && jumpCount < 2)
-            {
-                if (jumpCount == 1)
-                {
-                    //Двойной прыжок
-                    if (rb.velocity.y < 1.0f)
-                        Tap();
-                    return;
-                }
-
-                //Первый прыжок
-                Tap();
-                return;
-            }
-        }
+        velocityVector = new Vector3(0.0f, yVelocity, 0.0f);
     }
 
-    private void Tap()
+    private void Update()
     {
-        if (isGrounded)
-            isGrounded = false;
-        rb.velocity = velocityVector;
-        jumpCount++;
-    }
-
-    private void Holding(Touch touch)
-    {
-        if (touch.phase == TouchPhase.Began)
+        if(Input.GetMouseButtonDown(0) && jumpCount < 2)
         {
-            timer++;
+            Jump();
+            return;
         }
 
-        if (timer > 1.0f && rb.velocity.y < 1.0f)
+        if(jumpCount == 2 && Input.GetMouseButtonDown(0))
         {
-            Physics.gravity = new Vector3(0.0f, -1.0f, 0.0f);
-            rb.velocity = new Vector3(0.0f, -1.0f, 0.0f);
+            Physics.gravity = new Vector3(0.0f, -0.75f, 0.0f);
+            rigidbody.velocity = new Vector3(0.0f, -0.5f, 0.0f);
         }
 
-        if (touch.phase == TouchPhase.Ended)
+        if(jumpCount == 2 && Input.GetMouseButtonUp(0) && Physics.gravity.y != -9.81f)
         {
             Physics.gravity = new Vector3(0.0f, -9.81f, 0.0f);
         }
     }
 
-    private bool isGround()
+    private void Jump()
     {
-        if (transform.position.y < -3.05f && rb.velocity.y < 0.0f)
-            return true;
-        return false;
+        rigidbody.velocity = velocityVector;
+        jumpCount++;
     }
 
-    private void Landing()
+    private void OnCollisionEnter(Collision collision)
     {
-        Physics.gravity = new Vector3(0.0f, -9.81f, 0.0f);
-        isGrounded = true;
-        jumpCount = 0;
-        timer = 0.0f;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            jumpCount = 0;
+
+            if(Physics.gravity.y != -9.81f)
+            {
+                Physics.gravity = new Vector3(0.0f, -9.81f, 0.0f);
+            }
+        }
     }
 }
